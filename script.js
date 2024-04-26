e4fcd97e66041cbf3d0a74a9b69b7db32ca1db47
Vyshnavi1986 <157476467+Vyshnavi1986@users.noreply.github.com>
2024-04-26 23:06:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function getCurrentURLLength() {
+    // Get the current URL
+    const currentURL = window.location.href;
+
+    // Get the length of the current URL
+    const urlLength = currentURL.length;
+
+    // Return the length of the current URL
+    return urlLength;
+}
+// Print the length of the current URL
+console.log("Length of the current URL:", getCurrentURLLength());
